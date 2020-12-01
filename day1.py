f9ccc6b14c29ff7a55075f0ad0b8851c5f6a200c
Nat Peterson <natpeterson@gmail.com>
2020-12-01 07:48:13 -0800

    little faster...

--- day1.py
+++ day1.py
@@ -14,7 +14,7 @@ sample = """\
 1456
 """.splitlines()
 
-numbers = list(map(int,[line.strip() for line in lines]))
+numbers = list(sorted(map(int,[line.strip() for line in lines])))
 
 
 def part1(numbers):
@@ -23,6 +23,12 @@ def part1(numbers):
 			if x+y==2020:
 				return x*y
 
+def part1faster(number):
+	for i in range(len(numbers)):
+		for j in range(i+1, len(numbers)):
+				if numbers[i]+numbers[j]==2020:
+					return numbers[i]*numbers[j]
+
 def part2(numbers):
 	for x in numbers:
 		for y in numbers:
@@ -30,6 +36,14 @@ def part2(numbers):
 				if x+y+z==2020:
 					return x*y*z
 
+def part2faster(number):
+	for i in range(len(numbers)):
+		for j in range(i+1, len(numbers)):
+			if numbers[i]+numbers[j] >= 2020:
+				continue
+			for k in range(j+1, len(numbers)):
+				if numbers[i]+numbers[j]+numbers[k]==2020:
+					return numbers[i]*numbers[j]*numbers[k]
 
-print("#1", part1(numbers))
-print("#1", part2(numbers))
+print("#1", part1faster(numbers))
+print("#1", part2faster(numbers))
