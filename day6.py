c8d5afeb578e18496f3bad80897140adc069efef
Nat Peterson <natpeterson@gmail.com>
2020-12-06 10:11:50 -0800

    day6 now with set ops

--- day6.py
+++ day6.py
@@ -1,56 +1,25 @@
 #!/usr/bin/env python3
 
+def parse_groups(lines):
+    """returns a list of groups of voters and their votes"""
+    groups = list()
+    groups.append(list())  # first group
+    for line in lines:
+        if not line:  # next group
+            groups.append(list())
+        else:
+            groups[-1].append(line)
+    return groups
 
 with open("input6") as fp:
     input_lines = [line.strip() for line in fp.readlines()]
 
+groups = parse_groups(input_lines)
 
-sample = """\
-abc
+# votes made by at least one member in a group
+part1 = sum([len(set().union(*[set(person) for person in group])) for group in groups])
+print("#1", part1)
 
-a
-b
-c
-
-ab
-ac
-
-a
-a
-a
-a
-
-b
-""".splitlines()
-
-groups = list()
-group = set()
-for line in input_lines:
-    if not line:
-        groups.append(group)
-        group = set()
-    else:
-        for c in line:
-            group.add(c)
-groups.append(group)
-
-print("#1",sum(len(group) for group in groups))
-
-groups = list()
-group = list()
-for line in input_lines:
-    if not line:
-        groups.append(group)
-        group = list()
-    else:
-        group.append(line)
-groups.append(group)
-
-def common(group):
-    chars = set()
-    for person in group:
-        for c in person:
-            chars.add(c)
-    return sum((1 for c in chars if all(c in person for person in group)))
-
-print("#2",sum((common(group) for group in groups)))
+# votes made by all members of a group
+part2 = sum([len(set.intersection(*[set(person) for person in group])) for group in groups])
+print("#2", part2)
