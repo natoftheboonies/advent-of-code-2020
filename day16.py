f11d9b9f9b6c374f7461579c3ad92b18b4c2e724
Nat Peterson <natpeterson@gmail.com>
2020-12-16 08:01:37 -0800

    day16 part2

--- day16.py
+++ day16.py
@@ -4,38 +4,85 @@ import re
 
 def parse(data):
     sections = data.split("\n\n")
-    ranges = []
+    fields = dict()  # class : ((0,1),(4,9))
     for line in sections[0].splitlines():
+        field = line.split(":")[0]
+        fieldranges = []
         matches = re.findall(r"(\d+-\d+)",line.strip())
         for match in matches:
             low, high = match.split("-")
-            ranges.append((int(low),int(high)))
+            fieldranges.append((int(low),int(high)))
+        fields[field] = tuple(fieldranges)
+
+    myticket = tuple(map(int,sections[1].splitlines()[1].strip().split(",")))
 
     nearby = sections[2].splitlines()[1:]
-    fieldvals = []
+    tickets = []
     for line in nearby:
-        fieldvals.extend(map(int,line.strip().split(",")))
+        ticket = tuple(map(int,line.strip().split(",")))
+        tickets.append(ticket)
 
-    return ranges, fieldvals
+    return fields, myticket, tickets
+
+def find_invalid(fields, ticket):
+    invalid = []
+    for f in ticket:
+        for field in fields.values():
+            if any(r[0] <= f <= r[1] for r in field):
+                break
+        else:
+            invalid.append(f)
+    return invalid
 
 
 def part1(data):
-    ranges, fieldvals = parse(data)
+    fields, myticket, tickets = parse(data)
     invalid = []
-    for v in fieldvals:
-        for r in ranges:
-            if r[0] <= v <= r[1]:
-                break
-        else:
-            invalid.append(v)
-
+    for t in tickets:
+        invalid.extend(find_invalid(fields,t))
     return sum(invalid)
 
+def valid_fields(fields, tickets, idx):
+    valid = []
+    for field, ranges in fields.items():
+        for ticket in tickets:
+            value = ticket[idx]
+            if not any(r[0] <= value <= r[1] for r in ranges):
+                break
+        else:
+            valid.append(field)
+    return valid
+
+
+def part2(data):
+    fields, myticket, tickets = parse(data)
+    alltickets = [t for t in tickets if not find_invalid(fields, t)]+[myticket]
+    possible = []
+    for i in range(len(myticket)):
+        valid = valid_fields(fields, alltickets, i)
+        possible.append(valid)
+    count = 0
+    while any((len(p)>1 for p in possible)) and count < 100:
+        count+=1
+        # find certain fields:
+        certain = [p[0] for p in possible if len(p) == 1]
+        for p in possible:
+            if len(p)==1:
+                continue
+            for c in certain:
+                if c in p:
+                    p.remove(c)
+    departure_product = 1
+    for i, p in enumerate(possible):
+        if p[0].startswith("departure"):
+            departure_product *= myticket[i]
+    return departure_product
 
 with open('input16') as fp:
     puzzle = fp.read()
 
 print("#1",part1(puzzle))
+print("#2",part2(puzzle))
 
 sample = """\
 class: 1-3 or 5-7
@@ -53,3 +100,18 @@ nearby tickets:
 """
 
 assert part1(sample)==71
+
+sample2 = """\
+class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+"""
+part2(sample2)
