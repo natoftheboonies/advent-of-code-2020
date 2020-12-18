8c7acb8c7506689c6ec63b2c0d3dfd383f74259f
Nat Peterson <natpeterson@gmail.com>
2020-12-18 14:34:52 -0800

    tidy regex

--- day18.py
+++ day18.py
@@ -4,60 +4,52 @@ import re
 
 
 def newmath(expr):
-    result = None
     e = expr.split()
     result = int(e[0])
     for i in e[1:]:
         if not i.isnumeric():
             op = i
         else:
-            result = eval(str(result)+op+i)
+            result = eval(str(result) + op + i)
     return result
+
 
 assert newmath("1 + 2 * 3 + 4 * 5 + 6") == 71
 
-def plusfirst(expr):
-    matches = list(re.finditer(r"(\d+ \+ \d+)", expr))
-    while matches:
-        for match in reversed(matches):
-            #print(match, "=", newmath(match.group(0)))
-            expr = expr[:match.start()]+str(newmath(match.group(0)))+expr[match.end():]
-            #print(".",expr)
-        matches = list(re.finditer(r"(\d+ \+ \d+)", expr))
 
-    assert '+' not in expr
+def part1(expr, part2=False):
+    p = re.compile(r"\(([^()]+)\)")
+    while p.search(expr):
+        expr = p.sub(lambda m: str(newmath(m.group(1))), expr)
     return newmath(expr)
 
-def part1(expr,part2=False):
-    matches = re.findall(r"(\([^()]+\))", expr)
-    while matches:
-        for match in matches:
-            #print(match, "=", newmath(match[1:-1]))
-            if part2:
-                expr = expr.replace(match,str(plusfirst(match[1:-1])))
-            else:
-                expr = expr.replace(match,str(newmath(match[1:-1])))
-        matches = re.findall(r"(\([^()]+\))", expr)
-    if part2:
-        return plusfirst(expr)
-    else:
-        return newmath(expr)
 
-assert part1("1 + (2 * 3) + (4 * (5 + 6))") == 51
-assert part1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")==12240
+def part2(expr):
+    def plusfirst(expr):
+        p = re.compile(r"(\d+ \+ \d+)")
+        while p.search(expr):
+            expr = p.sub(lambda m: str(eval(m.group(1))), expr)
+        return eval(expr)
 
-with open('input18') as fp:
+    p = re.compile(r"\(([^()]+)\)")
+    while p.search(expr):
+        expr = p.sub(lambda m: str(plusfirst(m.group(1))), expr)
+    return plusfirst(expr)
+
+
+with open("input18") as fp:
     lines = [line.strip() for line in fp.readlines()]
 
-result = sum(part1(line) for line in lines)
-print("#1",result)
-
-assert part1("1 + 2 * 3 + 4 * 5 + 6",True)==231
-assert part1("1 + (2 * 3) + (4 * (5 + 6))",True)==51
-assert part1("2 * 3 + (4 * 5)",True)==46
-assert part1("5 + (8 * 3 + 9 + 3 * 4 * 3)",True)==1445
-assert part1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",True)==669060
-assert part1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",True)==23340
+print("#1", sum(part1(line) for line in lines))
+print("#2", sum(part2(line) for line in lines))
 
 
-print("#2",sum(part1(line,True) for line in lines)) # not 535809599856883
+assert part1("1 + (2 * 3) + (4 * (5 + 6))") == 51
+assert part1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240
+
+assert part2("1 + 2 * 3 + 4 * 5 + 6") == 231
+assert part2("1 + (2 * 3) + (4 * (5 + 6))") == 51
+assert part2("2 * 3 + (4 * 5)") == 46
+assert part2("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 1445
+assert part2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 669060
+assert part2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 23340
