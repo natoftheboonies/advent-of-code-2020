d9471f1ae3e075042a1db2f6ba816d7392bb8d6b
Nat Peterson <natpeterson@gmail.com>
2020-12-23 10:55:07 -0800

    part2 yaaaay

--- day23.py
+++ day23.py
@@ -23,33 +23,16 @@ class Cup(object):
 
     def findend(self):
         search = self.ahead
+        count = 0
         if not search:
             return self
         else:
             while search.ahead and search != self:
+                count += 1
                 search = search.ahead
+            assert count < 3
             return search
 
-
-    def destination(self):
-        # look for label-1 down to 0, or find max
-        # this is slow.  need up to 3 targets or max-3
-        target = self.label-1
-        while target > 0:
-            search = self.ahead
-            while search != self:
-                if search.label == target:
-                    print("found destination",search)
-                    return search
-                search = search.ahead
-            target -= 1
-        maxcup = search = self.ahead
-        while search != self:
-            if search.label > maxcup.label:
-                maxcup = search
-            search = search.ahead
-        print("max destination",maxcup)
-        return maxcup
 
 
     def tolist(self):
@@ -68,6 +51,7 @@ class Cup(object):
             if search.label == label:
                 return search
             search = search.ahead
+        return None
 
 
     def __str__(self):
@@ -102,21 +86,27 @@ def part1(puzzle, moves):
 
     for _ in range(moves):
         pick3 = current.pick3()
-        destination = 0
-        for target in range(current.label-1, current.label-4,-1):
-            if target < 1 or pick3.findcup(target):
-                continue
-            destination = target
-            break
-        if not destination:
-            for target in range(maxcup, maxcup-4,-1):
-                if not pick3.findcup(target):
-                    destination = target
-                    break
+        destination = current.label - 1
+        while destination in (pick3.label, pick3.ahead.label, pick3.ahead.ahead.label):
+            destination -= 1
+        if destination < 1:
+            destination = maxcup
+            while destination in (pick3.label, pick3.ahead.label, pick3.ahead.ahead.label):
+                destination -= 1
+        # for target in range(current.label-1, current.label-4,-1):
+        #     if target < 1 or pick3.findcup(target) is not None:
+        #         continue
+        #     destination = target
+        #     break
+        # if not destination:
+        #     for target in range(maxcup, maxcup-4,-1):
+        #         if not pick3.findcup(target) is not None:
+        #             destination = target
+        #             break
         cupmap[destination].insert(pick3)
         # move ahead
         current = current.ahead
-    return ''.join(str(i) for i in current.findcup(1).tolist()[1:])
+    return ''.join(str(i) for i in cupmap[1].tolist()[1:])
 
 
 def part2(puzzle):
@@ -140,36 +130,36 @@ def part2(puzzle):
         lastcup = cup
     lastcup.ahead = firstcup
     current = firstcup
+    # print("first",firstcup)
+    # print("last",lastcup)
     assert len(cupmap.keys())==maxcup
     for x in range(10000000):
         pick3 = current.pick3()
-        destination = 0
-        for target in range(current.label-1, current.label-4,-1):
-            if target < 1 or pick3.findcup(target):
-                continue
-            destination = target
-            break
-        if not destination:
-            for target in range(maxcup, maxcup-3,-1):
-                if not pick3.findcup(target):
-                    destination = target
-                    break
-        # destination is
+        destination = current.label - 1
+        while destination in (pick3.label, pick3.ahead.label, pick3.ahead.ahead.label):
+            destination -= 1
+        if destination < 1:
+            destination = maxcup
+            while destination in (pick3.label, pick3.ahead.label, pick3.ahead.ahead.label):
+                destination -= 1
+            # for target in range(maxcup, maxcup-3,-1):
+            #     if not pick3.findcup(target):
+            #         destination = target
+            #         break
         #print("dest",destination,"for current",current.label)
         cupmap[destination].insert(pick3)
-        #current.findcup(destination).insert(pick3)
         # move ahead
         current = current.ahead
     cup1 = cupmap[1]
-    print("1 seq",cup1,cup1.ahead,cup1.ahead.ahead)
+    #print("1 seq", cup1, cup1.ahead, cup1.ahead.ahead)
     return cup1.ahead.label*cup1.ahead.ahead.label
 
 puzzle = "193467258"
 print("#1",part1(puzzle,100))
-#print("#2",part2(puzzle)) # 744529654 too low, 602154808356 too high
+print("#2",part2(puzzle)) # 744529654 too low, 602154808356 too high
 
 sample = "389125467"
 assert part1(sample,10)=="92658374"
 assert part1(sample,100)=="67384529"
-assert part2(sample)==149245887792
+#assert part2(sample)==149245887792
 
